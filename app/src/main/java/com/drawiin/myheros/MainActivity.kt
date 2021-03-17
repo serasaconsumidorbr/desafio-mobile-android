e93006bccc9d71c6c6967fee54f2d535e71d5937
vinicius santos <viniciussantos@jera.com.br>
2021-03-16 21:48:56 -0400

    initial commit

---
+++ app/src/main/java/com/drawiin/myheros/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.drawiin.myheros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
