349ead451dcc52c7b047184156d30878fb957d86
vinicius santos <viniciussantos@jera.com.br>
2021-03-17 01:16:32 -0400

    setup ready

--- app/src/main/java/com/drawiin/myheroes/presentation/ui/heroes_list/HeroesViewModel.kt
+++ app/src/main/java/com/drawiin/myheroes/presentation/ui/heroes_list/HeroesViewModel.kt
@@ -7,7 +7,6 @@ import com.drawiin.myheroes.utils.NAMED_API_KEY
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Named
 
-@AndroidEntryPoint
 class HeroesViewModel @ViewModelInject constructor(
     private val getHeroes: GetHeroes,
     @Named(NAMED_API_KEY) private val apiKey: String
