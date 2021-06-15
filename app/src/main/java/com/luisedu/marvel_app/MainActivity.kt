b34bbbf9874b8b4ff182ac57952167eae7f28d9d
Luis Eduardo <luiseduardofp0070@gmail.com>
2021-06-15 11:28:00 -0300

    Início/configuração do projeto

---
+++ app/src/main/java/com/luisedu/marvel_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.luisedu.marvel_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
