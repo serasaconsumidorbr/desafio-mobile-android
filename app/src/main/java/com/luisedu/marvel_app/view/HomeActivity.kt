e0bacf34f3007d416be9a35a57a360a39e6b2742
Luis Eduardo <luiseduardofp0070@gmail.com>
2021-06-15 11:37:08 -0300

    Implementação dos layouts feita

---
+++ app/src/main/java/com/luisedu/marvel_app/view/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.luisedu.marvel_app.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import com.luisedu.marvel_app.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
