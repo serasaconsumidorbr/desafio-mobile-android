f2c5a13d1d9e85da86d56bc0430f9f5351eb6c19
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-20 08:10:29 -0300

    :sparkles: Iniciando Unificação das dependencias
    Iniciando

--- app/src/main/java/com/br/leandro/marvel_hero_app/ui/MainActivity.kt
+++ app/src/main/java/com/br/leandro/marvel_hero_app/ui/MainActivity.kt
@@ -1,35 +1,34 @@
-package com.br.leandro.marvel_hero_app
+package com.br.leandro.marvel_hero_app.ui
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.br.leandro.marvel_hero_app.R
 import com.br.leandro.marvel_hero_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_favorites,
+                R.id.navigation_hero
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+
+
 }
