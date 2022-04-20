76979754362f703b69fe2f74ee9e73846d95253c
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-20 09:02:07 -0300

    :fire: Finalizado parte home
    Home

--- app/src/main/java/com/br/leandro/marvel_hero_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/br/leandro/marvel_hero_app/ui/home/HomeFragment.kt
@@ -1,42 +1,101 @@
 package com.br.leandro.marvel_hero_app.ui.home
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.br.leandro.marvel_hero_app.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>(), OnFavoriteButtonClick {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    override val mViewModel: HomeViewModel by navGraphViewModels(R.id.mobile_navigation) { mViewModelFactory }
 
+    private val homeHeroListAdapter: HomeHeroListAdapter by lazy {
+        HomeHeroListAdapter(onFavoriteButtonClick = this, requestManager = requestManagerGlide)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    ): View? {
+        return mViewBinding.root
+    }
+    override fun setupViewModel() {
+        mViewModel.heroList.observe(viewLifecycleOwner, Observer { heroList ->
+            heroList.forEachIndexed { index, hero ->
+                Log.d("Hero", "$index -> $hero.id")
+            }
+            if (heroList.isNotEmpty()) {
+                homeHeroListAdapter.submitList(heroList)
+                changeToHeroesLayout(mViewBinding.viewSwitcherHomeScreen.displayedChild)
+            } else {
+                mViewBinding.viewSwitcherHomeScreen.displayedChild = VILLAIN_LAYOUT_INTERNET_PROBLEM
+            }
+        })
+        if (mViewModel.heroList.value.isNullOrEmpty())
+            mViewModel.getListOfHeroes()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun setupView() {
+        mViewBinding.homeHeroRecyclerView.apply {
+            layoutManager = LinearLayoutManager(fragmentContext)
+            adapter = homeHeroListAdapter
+            addOnScrollListener(object :
+                InfiniteRecyclerViewScrollListener(this.layoutManager as LinearLayoutManager) {
+                override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                    mViewModel.getListOfHeroes(Companion.OFF_SET_TO_LOAD_MORE_HEROES_PER_PAGE * page)
+                }
+            })
+        }
+
+        mViewBinding.fabRetryLoadingHeroes.setOnClickListener {
+            mViewModel.getListOfHeroes()
+            mViewBinding.searchHero.setText("")
+        }
+
+        mViewBinding.searchHero.apply {
+            addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+
+                override fun beforeTextChanged(s: CharSequence?, st: Int, c: Int, a: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, st: Int, bf: Int, ct: Int) {
+                    mViewModel.filterHeroByName(s.toString())
+                }
+            })
+            setText(mViewModel.latestSearch)
+        }
+    }
+
+    override fun getViewBinding(): FragmentHomeBinding {
+        return FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onFavoriteClicked(hero: Hero, shouldSave: Boolean) {
+        if (shouldSave)
+            mViewModel.saveFavoriteHero(hero)
+        else
+            mViewModel.deleteFavoriteHero(hero)
+    }
+
+    private fun changeToHeroesLayout(childDisplayed: Int) {
+        if (childDisplayed == VILLAIN_LAYOUT_INTERNET_PROBLEM) {
+            mViewBinding.viewSwitcherHomeScreen.displayedChild = HERO_LAYOUT_INTERNET_SUCCESS
+        }
+    }
+
+    companion object {
+        private const val OFF_SET_TO_LOAD_MORE_HEROES_PER_PAGE = 5
+        private const val VILLAIN_LAYOUT_INTERNET_PROBLEM = 0
+        private const val HERO_LAYOUT_INTERNET_SUCCESS = 1
     }
 }
