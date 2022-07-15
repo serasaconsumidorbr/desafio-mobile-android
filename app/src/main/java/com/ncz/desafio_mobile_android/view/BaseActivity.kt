031270940ab8961dc1e7b3cd1d09b8c9b609278f
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-15 00:20:26 -0300

    configured the navigation in the project and styled the Splash View

--- app/src/main/java/com/ncz/desafio_mobile_android/view/BaseActivity.kt
+++ app/src/main/java/com/ncz/desafio_mobile_android/view/BaseActivity.kt
@@ -1,11 +1,12 @@
-package com.ncz.desafio_mobile_android
+package com.ncz.desafio_mobile_android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ncz.desafio_mobile_android.R
 
 class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_base)
     }
 }
---
+++ app/src/main/java/com/ncz/desafio_mobile_android/view/home/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.ncz.desafio_mobile_android.view.home
+
+import androidx.fragment.app.Fragment
+import com.ncz.desafio_mobile_android.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+
+}
