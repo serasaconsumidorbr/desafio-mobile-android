7bb9c269ca2df0f626fe3cca16c5f8717a711281
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-14 23:41:21 -0300

    splash screen creation

--- app/src/main/java/com/ncz/desafio_mobile_android/BaseActivity.kt
+++ app/src/main/java/com/ncz/desafio_mobile_android/BaseActivity.kt
@@ -3,7 +3,7 @@ package com.ncz.desafio_mobile_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
