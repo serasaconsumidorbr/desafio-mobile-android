c9055a91d9e68365318eff6e6d67ff3cd86128b6
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-16 17:05:18 -0300

    feature(CharacterAdapter):creating the Character Adapter

---
+++ app/src/main/java/com/ncz/desafio_mobile_android/view/adapters/CharactersAdapter.kt
@@ -0,0 +1,32 @@
+package com.ncz.desafio_mobile_android.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ncz.desafio_mobile_android.databinding.CardCharacterItemBinding
+import com.ncz.desafio_mobile_android.domain.entities.character.Character
+
+class CharactersAdapter(private val characterList: List<Character>) :
+    RecyclerView.Adapter<CharactersAdapter.ViewHolderCharacter>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderCharacter {
+        val binding =
+            CardCharacterItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false)
+        return ViewHolderCharacter(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderCharacter, position: Int) {
+      holder.name.text = characterList[position].name
+    }
+
+    override fun getItemCount() = characterList.size
+
+    inner class ViewHolderCharacter(binding: CardCharacterItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val name = binding.txtName
+    }
+
+}
