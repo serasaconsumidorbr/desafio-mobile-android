8ca1f8bc80ba997e6c5037660dcab26d0f347e2b
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-17 19:06:11 -0300

    HotFix(Detail): removing unnecessary classes at the moment

--- app/src/main/java/com/ncz/desafio_mobile_android/app/view/detail/DetailFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.ncz.desafio_mobile_android.app.view.detail
-
-import androidx.fragment.app.Fragment
-import com.ncz.desafio_mobile_android.R
-
-class DetailFragment : Fragment(R.layout.fragment_detail) {
-}
