2b3e7bae852f510bb7dd402ca12b9f751f8a720b
Thiago Filadelfo <thiago.filadelfo>
2022-07-30 18:12:19 -0300

    feat(app): iniciando projeto

---
+++ app/src/main/java/com/developer/marvel/app/home/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.developer.marvel.app.home
+
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity: AppCompatActivity()
---
+++ app/src/main/java/com/developer/marvel/app/home/BaseFragment.kt
@@ -0,0 +1,5 @@
+package com.developer.marvel.app.home
+
+import androidx.fragment.app.Fragment
+
+class BaseFragment: Fragment()
