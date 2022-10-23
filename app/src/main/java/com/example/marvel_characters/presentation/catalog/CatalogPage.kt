7db94e41626c8fc6e12881d4f636a132cd4df29c
josebiasneto <josebiasbsneto@gmail.com>
2022-10-23 09:45:09 -0300

    build details page, loading and error handling

--- app/src/main/java/com/example/marvel_characters/presentation/catalog/CatalogPage.kt
+++ app/src/main/java/com/example/marvel_characters/presentation/catalog/CatalogPage.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import coil.compose.AsyncImage
 import com.example.marvel_characters.domain.models.Characters
 import com.example.marvel_characters.presentation.catalog.components.DotsIndicator
@@ -39,6 +43,7 @@ class CatalogPage: Fragment() {
         setContent {
             headerList = viewModel.headerList
             bodyList = viewModel.bodyList
+            isNetworkAvailable = viewModel.isNetworkAvailable.value
 
             val state = rememberPagerState()
 
@@ -50,7 +55,15 @@ class CatalogPage: Fragment() {
                             modifier = Modifier.padding(25.dp)) {
                             if(headerList.size > page){
                                 AsyncImage(model = headerList[page].image,
-                                    contentDescription = headerList[page].name)
+                                    contentDescription = headerList[page].name,
+                                modifier = Modifier.clickable {
+                                    goToDetailsPage(headerList[page])
+                                })
+                            } else {
+                                Spacer(modifier = Modifier.padding(50.dp))
+                                CircularProgressIndicator(modifier = Modifier.size(50.dp),
+                                    color = Color.Red)
+                                Spacer(modifier = Modifier.padding(50.dp))
                             }
                         }
                     }
@@ -71,10 +84,23 @@ class CatalogPage: Fragment() {
                 items(bodyList.size){ index ->
                     AsyncImage(model = bodyList[index].image,
                         contentDescription = bodyList[index].name,
-                        modifier = Modifier.padding(10.dp))
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .clickable {
+                                goToDetailsPage(bodyList[index])
+                            })
                 }
+            }
 
+            if(!isNetworkAvailable){
+                Toast.makeText(requireContext(), "Servidor indisponivel, verifique a conexao!",
+                    Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    private fun goToDetailsPage(character: Characters){
+        requireView().findNavController().navigate(
+            CatalogPageDirections.actionCatalogFragmentToDetailsFragment(character))
     }
 }
--- app/src/main/java/com/example/marvel_characters/presentation/details/DetailsPage.kt
+++ app/src/main/java/com/example/marvel_characters/presentation/details/DetailsPage.kt
@@ -4,9 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import coil.compose.AsyncImage
+import com.example.marvel_characters.R
 import com.example.marvel_characters.domain.models.Characters
 
 class DetailsPage: Fragment() {
@@ -22,6 +33,21 @@ class DetailsPage: Fragment() {
         setContent {
             character = args.character
 
+            Column {
+                IconButton(onClick = { requireActivity().onBackPressed() },
+                modifier = Modifier.padding(start = 20.dp, top = 10.dp)){
+                    Icon(painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
+                        contentDescription = "Voltar", Modifier.size(30.dp))
+                }
+                AsyncImage(model = character.image,
+                    contentDescription = character.name,
+                    modifier = Modifier.padding(30.dp))
+                Text(text = character.name, color = Color.Black,
+                fontSize = 24.sp, modifier = Modifier.padding(start = 30.dp))
+                Text(text = character.description, color = Color.Gray,
+                fontSize = 22.sp, modifier = Modifier
+                        .padding(horizontal = 30.dp).fillMaxWidth())
+            }
         }
     }
 }
