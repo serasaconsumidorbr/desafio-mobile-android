e4112632665ddae582aa69e22b55aef8186ab2d6
vcoelho <vcoelho@isolutions.com.br>
2022-11-19 19:53:22 -0300

    added splashscreen

---
+++ app/src/main/java/br/com/marvel/presentation/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package br.com.marvel.presentation.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import br.com.marvel.presentation.character.CharacterActivity
+import br.com.marvel.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, CharacterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
