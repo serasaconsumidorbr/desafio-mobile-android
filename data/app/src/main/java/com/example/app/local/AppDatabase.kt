4777b7649fd0f9ed847efbace70f90d9785e4e33
Guilherme Grijó <guilhermegrijo@outlook.com>
2023-01-22 10:32:22 -0300

    inicio navegação

--- data/app/src/main/java/com/example/app/local/AppDatabase.kt
+++ data/app/src/main/java/com/example/app/local/AppDatabase.kt
@@ -4,13 +4,17 @@ import android.app.Application
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.data.heroes.local.dao.HeroDao
 import com.example.data.heroes.local.entity.HeroEntity
+import com.example.data.heroes.local.entity.Thumbnail
+import com.google.gson.Gson
 
 private const val DATABASE_NAME = "app_database.db"
 private const val DATABASE_VERSION = 1
 
 @Database(entities = [HeroEntity::class], version = DATABASE_VERSION)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase(){
 
     abstract fun heroDao() : HeroDao
@@ -27,4 +31,12 @@ abstract class AppDatabase : RoomDatabase(){
         fun build(): AppDatabase = builder.build()
 
     }
+
+
+}
+
+private class Converters {
+    fun thumbnailtoJson(value: Thumbnail): String = Gson().toJson(value)
+
+    fun jsonToThumbnail(value: String) = Gson().fromJson(value, Thumbnail::class.java)
 }
