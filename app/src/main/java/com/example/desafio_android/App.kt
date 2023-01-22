bce99faf7d9a2968e6a3804f330d2124f9905a64
Guilherme Grijó <guilhermegrijo@outlook.com>
2023-01-22 20:39:17 -0300

    bug_fix database

--- app/src/main/java/com/example/desafio_android/App.kt
+++ app/src/main/java/com/example/desafio_android/App.kt
@@ -1,7 +1,30 @@
 package com.example.desafio_android
 
 import android.app.Application
+import android.os.Build
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .build()
+    }
+}
