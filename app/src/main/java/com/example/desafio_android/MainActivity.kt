c19ce11f7a420a26d34ccaf39cf21ba7b90c254c
Guilherme Grijó <guilhermegrijo@outlook.com>
2023-01-22 09:38:07 -0300

    inicio navegação

---
+++ app/src/main/java/com/example/desafio_android/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.desafio_android
+
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+}
