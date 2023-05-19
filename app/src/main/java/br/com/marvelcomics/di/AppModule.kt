e506e968f08d21ffa238b811e7379660c677bdc3
rodol <rodolfo__BN@hotmail.com>
2023-05-19 02:10:18 -0300

    paging3 library removed

--- app/src/main/java/br/com/marvelcomics/di/AppModule.kt
+++ app/src/main/java/br/com/marvelcomics/di/AppModule.kt
@@ -1,11 +1,8 @@
 package br.com.marvelcomics.di
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import br.com.marvelcomics.data.remote.MarvelApi
-import br.com.marvelcomics.data.remote.paging.MarvelCharacterPagingSource
+import br.com.marvelcomics.data.repository.MarvelCharRepository
+import br.com.marvelcomics.data.repository.MarvelCharRepositoryImpl
 import br.com.marvelcomics.feature.HomeViewModel
-import br.com.marvelcomics.model.MarvelCharacter
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -14,15 +11,9 @@ private const val PAGING_SIZE = 20
 object AppModule {
 
     val dependencies = module {
-        single { providePager(get()) }
+        single<MarvelCharRepository> { MarvelCharRepositoryImpl(get(), get()) }
 
         viewModel { HomeViewModel(get()) }
     }
 
-    private fun providePager(marvelApi: MarvelApi): Pager<Int, MarvelCharacter> {
-        return Pager(
-            config = PagingConfig(pageSize = PAGING_SIZE),
-            pagingSourceFactory = { MarvelCharacterPagingSource(marvelApi) }
-        )
-    }
 }
--- app/src/main/java/br/com/marvelcomics/di/DatabaseModule.kt
+++ app/src/main/java/br/com/marvelcomics/di/DatabaseModule.kt
@@ -2,6 +2,8 @@ package br.com.marvelcomics.di
 
 import android.content.Context
 import androidx.room.Room
+import br.com.marvelcomics.data.local.dao.MarvelDao
+import br.com.marvelcomics.data.local.db.MarvelComicsDatabase
 import org.koin.dsl.module
 
 object DatabaseModule {
@@ -9,12 +11,13 @@ object DatabaseModule {
     private const val DATABASE_NAME = "marvel_heroes_database"
 
     val dependencies = module {
-//        single<MarvelComicsDatabase> { provideDatabase(get()) }
+        single<MarvelComicsDatabase> { provideDatabase(get()) }
+        single<MarvelDao> { get<MarvelComicsDatabase>().marvelDao }
     }
 
 
-//    private fun provideDatabase(context: Context): MarvelComicsDatabase =
-//        Room.databaseBuilder(context, MarvelComicsDatabase::class.java, DATABASE_NAME)
-//            .fallbackToDestructiveMigration().build()
+    private fun provideDatabase(context: Context): MarvelComicsDatabase =
+        Room.databaseBuilder(context, MarvelComicsDatabase::class.java, DATABASE_NAME)
+            .fallbackToDestructiveMigration().build()
 
 }
