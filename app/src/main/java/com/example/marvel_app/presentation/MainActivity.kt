ade61118677b2e7e883d874932f038a09eea85cf
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-09-01 19:09:03 -0300

    configure navigation component

--- app/src/main/java/com/example/marvel_app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/presentation/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.marvel_app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.marvel_app.R
+import com.example.marvel_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
