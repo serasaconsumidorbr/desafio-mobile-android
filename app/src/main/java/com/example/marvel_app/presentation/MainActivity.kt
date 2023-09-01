bb00b181be756eafc6f5b1412606001ffd472b2d
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-09-01 20:14:24 -0300

    cofigure daggerhilt

--- app/src/main/java/com/example/marvel_app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/presentation/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.marvel_app.R
 import com.example.marvel_app.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
