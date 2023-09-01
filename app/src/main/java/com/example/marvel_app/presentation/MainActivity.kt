e1ef90db76e18f8a9038efe94ca640bb4dd6a9ea
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-09-01 19:00:58 -0300

    configure architecture

--- app/src/main/java/com/example/marvel_app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.marvel_app
+package com.example.marvel_app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvel_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
