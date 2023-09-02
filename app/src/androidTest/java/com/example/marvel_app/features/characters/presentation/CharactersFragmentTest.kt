9c81d9c2e5913fe56562453d8d9d5b17f57aea66
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-09-02 10:23:11 -0300

    configur CustomTestRunner

--- app/src/androidTest/java/com/example/marvel_app/features/characters/presentation/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvel_app/features/characters/presentation/CharactersFragmentTest.kt
@@ -2,12 +2,19 @@ package com.example.marvel_app.features.characters.presentation
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class CharactersFragmentTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
 
     @Before
     fun setup() {
