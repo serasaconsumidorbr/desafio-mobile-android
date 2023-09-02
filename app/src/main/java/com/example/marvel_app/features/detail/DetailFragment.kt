e31a3709ab8d850daf56e86e11ff2cf117961914
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-09-02 12:37:02 -0300

    create feature detail

---
+++ app/src/main/java/com/example/marvel_app/features/detail/DetailFragment.kt
@@ -0,0 +1,34 @@
+package com.example.marvel_app.features.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.marvel_app.R
+import com.example.marvel_app.databinding.FragmentCharactersBinding
+import com.example.marvel_app.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentDetailBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/marvel_app/features/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.marvel_app.features.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+
+}
