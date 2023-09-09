971fdaca092dce72b60d615a96390bb16d46f430
Ítalo Bezerra <italobezerra.cw@gmail.com>
2023-09-09 07:04:19 -0300

    Fixed file name

--- app/src/main/java/com/example/marvel_characters/ui/compose/MarvelCharactersApp.kt
+++ app/src/main/java/com/example/marvel_characters/ui/compose/MarvelCharactersApp.kt
@@ -2,9 +2,11 @@ package com.example.marvel_characters.ui.compose
 
 import android.content.res.Configuration
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,15 +38,23 @@ fun MarvelCharactersAppPreview() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OfflineDialog(onRetry: () -> Unit) {
+                var dismissState = rememberDismissState()
+
     AlertDialog(
-        onDismissRequest = {},
+        onDismissRequest = { },
         title = { Text(text = stringResource(R.string.internet_connection_error_title)) },
         text = { Text(text = stringResource(R.string.internet_connection_error_message)) },
 
         confirmButton = {
             TextButton(onClick = onRetry) {
+                Text(stringResource(R.string.retry_label))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = {}) {
                 Text(stringResource(R.string.retry_label))
             }
         }
