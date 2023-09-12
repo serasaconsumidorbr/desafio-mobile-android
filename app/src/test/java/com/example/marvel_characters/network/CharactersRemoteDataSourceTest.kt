13be48352c20a8a3f61f82a223111d810447abde
Ítalo Bezerra <italobezerra.cw@gmail.com>
2023-09-12 06:08:11 -0300

    Committed file code was changed back to an older version

--- app/src/test/java/com/example/marvel_characters/network/CharactersRemoteDataSourceTest.kt
+++ app/src/test/java/com/example/marvel_characters/network/CharactersRemoteDataSourceTest.kt
@@ -9,39 +9,16 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.isA
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
-import retrofit2.Response
-import java.io.File
-import java.nio.charset.StandardCharsets
+import org.koin.java.KoinJavaComponent
 
 class CharactersRemoteDataSourceTest: TestWithKoinBase() {
-    val listRequestBody =
-        readJsonFromFile("/src/test/java/com/example/marvel_characters/resources/successful-characters-list-response-body.json")
-
-    val pastRealSavedListResponse : Response<NetworkCharacterContainer> = Response.success(listRequestBody) as Response< NetworkCharacterContainer>
-    //val pastRealSavedCharacterList = pastRealSavedListResponse.body()!!.data
-
-    val characterRequestBody =         readJsonFromFile("/src/test/java/com/example/marvel_characters/resources/successful-character-response-body.json")
-    val characterAIMResponse = Response.success(characterRequestBody) as Response<NetworkCharacterContainer>
-    //val CharacterAIM = characterAIMResponse.body()!!.data
-
-
-    lateinit var marvelApiServiceMock: MarvelApiService
-
-    @Before
-    override fun setup() {
-        super.setup()
-        MockitoAnnotations.openMocks(this)
-        marvelApiServiceMock = Mockito.mock(MarvelApiService::class.java)
-    }
+    private val remoteDataSource: CharactersRemoteDataSource by KoinJavaComponent.inject(
+        CharactersRemoteDataSource::class.java
+    )
 
     @Test
     fun shouldReturnNonEmptyCharactersListResult(): Unit = runBlocking {
-        Mockito.`when`(marvelApiServiceMock.getCharacters(100, 0)).thenReturn(pastRealSavedListResponse)
-        val remoteDataSource  = CharactersRemoteDataSource(marvelApiServiceMock)
         val resultData = remoteDataSource.getNextCharacterPage()
 
         assertThat(resultData, isA(Result.Success(listOf<Character>()).javaClass))
@@ -50,21 +27,12 @@ class CharactersRemoteDataSourceTest: TestWithKoinBase() {
 
     @Test
     fun shouldReturnExpectCharactersData(): Unit = runBlocking {
-        Mockito.`when`(marvelApiServiceMock.getCharacters(100, 0)).thenReturn(characterAIMResponse)
-
         characterAIMWithCompleteData.let {
-            val remoteDataSource  = CharactersRemoteDataSource(marvelApiServiceMock)
-
             val resultData = remoteDataSource.getCharacterById(it.id)
+            assertThat(resultData, isA(Result.Success(listOf<Character>()).javaClass))
 
             val returnedCharacter = (resultData as Result.Success).data
             assertThat(returnedCharacter, equalTo(it))
         }
-    }
-
-    private fun readJsonFromFile(filePath: String): String {
-        val userDir = System.getProperty("user.dir")
-                val file = File(userDir + filePath)
-        return file.readText(StandardCharsets.UTF_8)
     }
 }
